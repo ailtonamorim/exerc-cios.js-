1ca4d2265269c9a9c0ef5e765a534563b3df14ae
aarsenelupin <amorimlima112@hotmail.com>
2026-02-06 10:10:16 -0300

    mais exercícios

---
+++ curso_js/livro.js/so.js
@@ -0,0 +1,13 @@
+    const a = 20.01;
+    const b = a * 2;
+    const c = a / 2;
+    const d = a - 2;
+    const e = Number(a) + 2;
+    // alert(" e " + e)
+    console.log(typeof e);
+    const f = toString(e);
+    console.log(Number.isInteger(a))
+
+    // console.log(typeof f)
+
+    // console.log(Namber.isInteger(100))
---
+++ curso_js/metodos/metodos.js
@@ -0,0 +1,6 @@
+let nome = "AILTON";
+
+console.log(nome);
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
+
